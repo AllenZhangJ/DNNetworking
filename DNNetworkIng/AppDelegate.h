8b8e5358db0a85b1eede3d8a8a1fc15a69ed8200
Allen <402769411@qq.com>
2017-04-14 16:29:01 +0800

    Initial Commit

---
+++ DNNetworkIng/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DNNetworkIng
+//
+//  Created by you hao on 2017/4/14.
+//  Copyright © 2017年 ChinaDNB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
